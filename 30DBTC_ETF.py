59f2625eb313505bf65fe990fbec28732371df0c
eekeyguy <128739145+eekeyguy@users.noreply.github.com>
2024-08-27 10:39:28 +0530

    Update 30DBTC_ETF.py

--- 30DBTC_ETF.py
+++ 30DBTC_ETF.py
@@ -5,26 +5,24 @@ from io import StringIO
 from datetime import datetime
 
 def fetch_etf_netflow_data():
-    url = "https://api.coinmarketcap.com/data-api/v3/etf/detail/netflow?category=btc&range=30d"
+    url = "https://api.coinmarketcap.com/data-api/v3/etf/overview/netflow?category=btc&range=30d"
     response = requests.get(url)
     return response.json()
 
 def extract_netflow_data(json_data):
     extracted_data = []
-    for etf in json_data['data']['points']:
-        etf_name = etf['name']
-        for point in etf['data']:
-            timestamp = datetime.fromtimestamp(int(point['timestamp']) / 1000).strftime('%Y-%m-%d')
-            extracted_data.append({
-                'date': timestamp,
-                'etf_name': etf_name,
-                'net_flow': point['value']
-            })
+    points = json_data['data']['points']
+    for point in points:
+        timestamp = datetime.fromtimestamp(int(point['timestamp']) / 1000).strftime('%Y-%m-%d')
+        extracted_data.append({
+            'date': timestamp,
+            'net_flow': point['value']
+        })
     return extracted_data
 
 def convert_to_csv(extracted_data):
     csv_file = StringIO()
-    fieldnames = ['date', 'etf_name', 'net_flow']
+    fieldnames = ['date', 'net_flow']
     csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
     csv_writer.writeheader()
     csv_writer.writerows(extracted_data)
